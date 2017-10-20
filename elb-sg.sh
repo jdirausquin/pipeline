87c4929c7f760ebcadcdb918f05417181882822e
JD Irausquin <jdirausquin@gmail.com>
2017-10-19 20:18:25 -0600

    Fixing Variables

--- elb-sg.sh
+++ elb-sg.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-terraform apply -var env=$Environment -var app=$Application_Name -var port=$Instance_Port -var clustersg=$1 -target aws_elb.elb -target aws_security_group_rule.rule_oncluster
+terraform apply -var env=$1 -var app=$2 -var port=$3 -var clustersg=$4 -target aws_elb.elb -target aws_security_group_rule.rule_oncluster
