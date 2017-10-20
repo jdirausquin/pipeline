bca1e8ef119b0f74109cc3f0ac75b97cfa64f61d
JD Irausquin <jdirausquin@gmail.com>
2017-10-19 19:15:34 -0600

    debug

--- get-sg.sh
+++ get-sg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SG=`aws ecs list-container-instances --cluster $1 | jq .containerInstanceArns[0] | xargs -I {} aws ecs describe-container-instances --cluster $1 --container-instances {} | jq .containerInstances[].ec2InstanceId | xargs -I {} aws ec2 describe-instances --instance-id {} | jq .Reservations[].Instances[].SecurityGroups[].GroupId`
-echo $SG
+echo $SG 2>&1
