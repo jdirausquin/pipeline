da9c1aab195326ab1161144faa28bcbf3ded8083
JD Irausquin <jdirausquin@gmail.com>
2017-10-19 21:38:17 -0600

    Updating pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,16 +1,23 @@
 pipeline {
     agent any
-     stage('Clone sources') {
-        git url: 'https://github.com/jdirausquin/pipeline.git'
-    }
     stages {
-        stage('Create ELB') {
+        stage('Clone source repo') {
             steps {
-                echo 'Getting SG of Cluster'
-                sh 'get-sg.sh $ClusterName > SG'
-                echo 'Creating ELB & Asigning rule to Cluster SG'
-                sh 'elb-sg.sh "${SG}"'
+                git url: 'https://github.com/jdirausquin/pipeline.git'
             }
+        }
+
+        stage('Create ELB') {
+                steps {
+                    echo 'Getting SG of Cluster'
+                    script {
+                        sh 'bash get-sg.sh $Cluster_Name > tmpsg'
+                        def SG=readFile('tmpsg')
+                        echo "Security group of Cluster: ${SG}"
+                        echo 'Creating ELB & Asigning rule to Cluster SG'
+                        sh "bash elb-sg.sh $Environment $Application_Name $Instance_Port ${SG}"
+                    }
+                }
         }
     }
 }
