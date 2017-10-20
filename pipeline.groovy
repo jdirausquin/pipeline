95b28810922c26a436280846526c7dc0ffa474e5
JD Irausquin <jdirausquin@gmail.com>
2017-10-19 18:13:36 -0600

    Initial commit 2

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,13 +1,15 @@
 pipeline {
     agent any
-
+     stage('Clone sources') {
+        git url: 'https://github.com/jdirausquin/pipeline.git'
+    }
     stages {
         stage('Create ELB') {
             steps {
                 echo 'Getting SG of Cluster'
                 sh 'get-sg.sh $ClusterName > SG'
                 echo 'Creating ELB & Asigning rule to Cluster SG'
-                sh 'elb-sg.sh'
+                sh 'elb-sg.sh "${SG}"'
             }
         }
     }
